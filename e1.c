cc9bb6b5834af8c8833586f356f92c9fef61a1c5
Roger Oba <rogerluan.oba@gmail.com>
2017-02-22 15:49:34 -0300

    Applied coding style (indentation)

--- e1.c
+++ e1.c
@@ -1,35 +1,35 @@
 /*******************************************************************************
-* Este programa esta baseado no segundo experimento do curso sobre tempo real 
-* do Laboratorio Embry-Riddle
-* 
-* Seguem os comentarios originais:
-*
-* Experiment #2: Multi-Tasking, Measuring Drift
-*
-*    Programmer: Eric Sorton
-*          Date: 1/27/97
-*           For: MSE599, Special Topics Class
-*
-*       Purpose: When a basic sleep call is used to determine the precise time
-*                when an action will occur the problem of drift occurs. 
-*                The measurement of time is imprecise.  Similarly, the time in 
-*                which the sleep call returns is imprecise.  Over time, this 
-*                will cause the ocurrence of time to drift.  Just as if a clock 
-*                loses 1 second every day, over one day, it is significant, but 
-*                over a year, it loses 365 seconds, which is over 6 minutes.  
-*                This is an example of drift.
-*
-*       Proposito: Quando uma chamada b�sica sleep e usada para determinar o
-*                instante exato em que alguma acao vai ocorrer, ocorre o problema
-*                do desvio. A medicao de tempo e imprecisa. Similarmente, o tempo
-*                que demora o retorno da chamada sleep tambem e impreciso. Ao
-*                longo do tempo, isto ocasionara um desvio de tempo. Algo como se
-*                um relogio perdesse um segundo a cada dia. Ao longo de um dia, 
-*                essa diferenca e insignificante, mas, ao longo de um ano, sao 
-*                perdidos 365 segundos, o que e superior a 6 minutos. Este e um
-*                exemplo de desvio.
-*
-*******************************************************************************/
+ * Este programa esta baseado no segundo experimento do curso sobre tempo real
+ * do Laboratorio Embry-Riddle
+ *
+ * Seguem os comentarios originais:
+ *
+ * Experiment #2: Multi-Tasking, Measuring Drift
+ *
+ *    Programmer: Eric Sorton
+ *          Date: 1/27/97
+ *           For: MSE599, Special Topics Class
+ *
+ *       Purpose: When a basic sleep call is used to determine the precise time
+ *                when an action will occur the problem of drift occurs.
+ *                The measurement of time is imprecise.  Similarly, the time in
+ *                which the sleep call returns is imprecise.  Over time, this
+ *                will cause the ocurrence of time to drift.  Just as if a clock
+ *                loses 1 second every day, over one day, it is significant, but
+ *                over a year, it loses 365 seconds, which is over 6 minutes.
+ *                This is an example of drift.
+ *
+ *       Proposito: Quando uma chamada b�sica sleep e usada para determinar o
+ *                instante exato em que alguma acao vai ocorrer, ocorre o problema
+ *                do desvio. A medicao de tempo e imprecisa. Similarmente, o tempo
+ *                que demora o retorno da chamada sleep tambem e impreciso. Ao
+ *                longo do tempo, isto ocasionara um desvio de tempo. Algo como se
+ *                um relogio perdesse um segundo a cada dia. Ao longo de um dia,
+ *                essa diferenca e insignificante, mas, ao longo de um ano, sao
+ *                perdidos 365 segundos, o que e superior a 6 minutos. Este e um
+ *                exemplo de desvio.
+ *
+ *******************************************************************************/
 
 /*
  * Includes Necessarios, verifique se as bibliotecas no diretorio sys/ estao
@@ -56,14 +56,14 @@
 
 /*
  * NO_OF_ITERATIONS e o numero de vezes que vai se repetir o loop existente
- * em cada futuro processo filho. 
+ * em cada futuro processo filho.
  */
 
 #define NO_OF_ITERATIONS	1000
 
 /*
  * NO_OF_CHILDREN eh o numero de filhos a serem criados, cada qual responsavel
- * pela medida do desvio. 
+ * pela medida do desvio.
  */
 
 #define NO_OF_CHILDREN	5
@@ -87,72 +87,72 @@
 
 int main( int argc, char *argv[] )
 {
-/*
- * Pergunta 4: o que s�o e para que servem argc e argv? N�o esqueca de
- * considerar o * antes de argv.
- */
-	/*
-	 * start_time e stop_time conterao o valor de tempo antes e depois
-	 * que as trocas de contexto comecem
+    /*
+     * Pergunta 4: o que s�o e para que servem argc e argv? N�o esqueca de
+     * considerar o * antes de argv.
+     */
+    /*
+     * start_time e stop_time conterao o valor de tempo antes e depois
+     * que as trocas de contexto comecem
+     */
+    
+    //struct timeval start_time;
+    //struct timeval stop_time;
+    
+    /*
+     * Outras variaveis importantes
+     */
+    
+    // float drift;
+    int count;
+    // int child_no;
+    
+    /*
+     * Criacao dos processos filhos
+     */
+    
+    pid_t rtn = 1;
+    for( count = 0; count < NO_OF_CHILDREN; count++ ) {
+        if( rtn > 0 ) {
+            rtn = fork();
+        } else {
+            break;
+        }
+    }
+    
+    
+    /*
+     * Verifica-se rtn para determinar se o processo eh pai ou filho
+     */
+    
+    if( rtn == 0 ) {
+        char countChar[256];
+        char iterationsChar[256];
+        char microChar[256];
+        char sleepChar[256];
+        
+        sprintf(countChar, "%d", count);
+        sprintf(iterationsChar, "%d", NO_OF_ITERATIONS);
+        sprintf(microChar, "%d", MICRO_PER_SECOND);
+        sprintf(sleepChar, "%d", SLEEP_TIME);
+        
+        char *my_args[6];
+        my_args[0] = "filho";
+        my_args[1] = countChar;
+        my_args[2] = iterationsChar;
+        my_args[3] = microChar;
+        my_args[4] = sleepChar;
+        my_args[5] = NULL;
+        execv ("filho", my_args);
+    } else {
+        /*
+         * Sou pai, aguardo o termino dos filhos
          */
-
-	//struct timeval start_time;
-	//struct timeval stop_time;
-
-      /*
-       * Outras variaveis importantes
-       */
-
-     // float drift;
-      int count;
-     // int child_no;
-
-	/*
-	 * Criacao dos processos filhos
-	 */
-
-	pid_t rtn = 1;
-	for( count = 0; count < NO_OF_CHILDREN; count++ ) {
-		if( rtn > 0 ) {
-			rtn = fork();
-		} else {
-			break;
-		}
-	}
-
-
-	/*
-	 * Verifica-se rtn para determinar se o processo eh pai ou filho
-	 */
-
-	if( rtn == 0 ) {
-		char countChar[256];
-		char iterationsChar[256];
-		char microChar[256];
-		char sleepChar[256];
-
-		sprintf(countChar, "%d", count);
-		sprintf(iterationsChar, "%d", NO_OF_ITERATIONS);
-		sprintf(microChar, "%d", MICRO_PER_SECOND);
-		sprintf(sleepChar, "%d", SLEEP_TIME);
-
-		char *my_args[6];
-		my_args[0] = "filho";
-  		my_args[1] = countChar;
-  		my_args[2] = iterationsChar;
-  		my_args[3] = microChar;
-  		my_args[4] = sleepChar;
-  		my_args[5] = NULL;
-  		execv ("filho", my_args);
-	} else {
-		/*
-		 * Sou pai, aguardo o termino dos filhos
-		 */
-		for( count = 0; count < NO_OF_CHILDREN; count++ ) {
-			wait(NULL);
-		}
-	}
-
-	exit(0);
+        for( count = 0; count < NO_OF_CHILDREN; count++ ) {
+            wait(NULL);
+        }
+    }
+    
+    exit(0);
 }
 
--- filho.c
+++ filho.c
@@ -6,68 +6,68 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-		int count;
-		int no_of_iterations;
-		int micro_per_second;
-		int sleep_time;
-
-		if (argc >= 4) {
-			sleep_time = atoi(argv[4]);	
-		}
-		if (argc >= 3) {
-			micro_per_second = atoi(argv[3]);	
-		}
-		if (argc >= 2) {
-			no_of_iterations = atoi(argv[2]);	
-		}
-		if (argc >= 1) {
-			count = atoi(argv[1]);	
-		}
-
-		printf("\n\n\nChild Number = %d\nIteractions = %d\nMicroPerSec = %d\nSleep Time = %d\n", count, no_of_iterations, micro_per_second, sleep_time);
-		
-		float drift;
+    int count;
+    int no_of_iterations;
+    int micro_per_second;
+    int sleep_time;
+    
+    if (argc >= 4) {
+        sleep_time = atoi(argv[4]);
+    }
+    if (argc >= 3) {
+        micro_per_second = atoi(argv[3]);
+    }
+    if (argc >= 2) {
+        no_of_iterations = atoi(argv[2]);
+    }
+    if (argc >= 1) {
+        count = atoi(argv[1]);
+    }
+    
+    printf("\n\n\nChild Number = %d\nIteractions = %d\nMicroPerSec = %d\nSleep Time = %d\n", count, no_of_iterations, micro_per_second, sleep_time);
+    
+    float drift;
   		int child_no;
-		struct timeval start_time;
-		struct timeval stop_time;
-		
-		child_no = count;
-
-		/*
-		 * Primeiro, obtenho o tempo inicial.
-		 */
-		gettimeofday( &start_time, NULL );
-
-	
-		/*
-		 * Este loop ocasiona a minha dormencia, de acordo com
-		 * SLEEP_TIME, tantas vezes quanto NO_OF_ITERATIONS
-		 */	
-		for( count = 0; count < no_of_iterations; count++ ) {
-			usleep(sleep_time);
-		}
-
-		/*
-		 * Paraobter o tempo final
-		 */
-		gettimeofday( &stop_time, NULL );
-
-		/*
-		 * Calcula-se o desvio
-		 */
-		drift = (float)(stop_time.tv_sec  - start_time.tv_sec);
-		drift += (stop_time.tv_usec - start_time.tv_usec)/(float)micro_per_second;
-
-		/*
-		 * Exibe os resultados
-		 */
-		printf("Filho #%d -- desvio total: %.5f -- desvio medio: %.5f\n",
-			child_no, drift - no_of_iterations*sleep_time/micro_per_second,
-			(drift - no_of_iterations *sleep_time/micro_per_second)/no_of_iterations);
-		
-		/*
-		 * Pergunta 5: Qual a relação: entre SLEEP_TIME e o desvio, nenhuma, direta 
-		 * ou indiretamente proporcional? 
-		 */
-		return 0;
+    struct timeval start_time;
+    struct timeval stop_time;
+    
+    child_no = count;
+    
+    /*
+     * Primeiro, obtenho o tempo inicial.
+     */
+    gettimeofday( &start_time, NULL );
+    
+    
+    /*
+     * Este loop ocasiona a minha dormencia, de acordo com
+     * SLEEP_TIME, tantas vezes quanto NO_OF_ITERATIONS
+     */
+    for( count = 0; count < no_of_iterations; count++ ) {
+        usleep(sleep_time);
+    }
+    
+    /*
+     * Paraobter o tempo final
+     */
+    gettimeofday( &stop_time, NULL );
+    
+    /*
+     * Calcula-se o desvio
+     */
+    drift = (float)(stop_time.tv_sec  - start_time.tv_sec);
+    drift += (stop_time.tv_usec - start_time.tv_usec)/(float)micro_per_second;
+    
+    /*
+     * Exibe os resultados
+     */
+    printf("Filho #%d -- desvio total: %.5f -- desvio medio: %.5f\n",
+           child_no, drift - no_of_iterations*sleep_time/micro_per_second,
+           (drift - no_of_iterations *sleep_time/micro_per_second)/no_of_iterations);
+    
+    /*
+     * Pergunta 5: Qual a relação: entre SLEEP_TIME e o desvio, nenhuma, direta 
+     * ou indiretamente proporcional? 
+     */
+    return 0;
 }
