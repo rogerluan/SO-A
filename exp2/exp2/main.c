4f9581058f26d01428324135b7b5a1bd8786e0df
Roger Oba <rogerluan.oba@gmail.com>
2017-03-13 13:40:47 -0300

    Fixed delta and max calculations, and coding style

---
+++ exp2/exp2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  exp2
+//
+//  Created by Roger Luan on 13/03/17.
+//  Copyright © 2017 Roger Oba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
