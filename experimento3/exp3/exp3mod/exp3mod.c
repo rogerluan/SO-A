21c5e0cf4d6a7285a2f39260b31cfca7e4533598
Roger Oba <rogerluan.oba@gmail.com>
2017-04-04 02:15:24 -0300

    Changed the sleep time

--- experimento3/exp3/exp3mod/exp3mod.c
+++ experimento3/exp3/exp3mod/exp3mod.c
@@ -116,6 +116,7 @@ int main(int argc, char *argv[]) {
         // Sleeps for a certain amount of time so the children can do their jobs.
         // After this time, kills the children, removes the semaphores and shared memories and exits the program.
         usleep(900000);
+        usleep(15000);
         
         // Killing the children
         int i;
