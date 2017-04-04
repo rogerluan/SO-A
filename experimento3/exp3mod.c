7c7fdafd3601059aae39b6c3732416f42259cf1c
Roger Oba <rogerluan.oba@gmail.com>
2017-04-04 03:02:30 -0300

    Organized files and xcode project

--- experimento3/exp3mod.c
+++ experimento3/exp3mod.c
@@ -178,7 +178,7 @@ int main(int argc, char *argv[]) {
     } else {
         // Sleeps for a certain amount of time so the children can do their jobs.
         // After this time, kills the children, removes the semaphores and shared memories and exits the program.
-        usleep(15000);
+        usleep(90000);
         
         // Killing the children
         int i;
