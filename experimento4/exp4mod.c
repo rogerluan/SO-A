7b68bb97e37097e0e1184e68cd65046a2c7cf65a
Roger Oba <rogerluan.oba@gmail.com>
2017-04-18 19:55:22 -0300

    Implemented exp4mod and it's not working

--- experimento4/exp4mod.c
+++ experimento4/exp4mod.c
@@ -9,69 +9,115 @@
 #define NUM_OF_PHILOSOPHERS 5
 #define NUM_OF_ITERATIONS 365
 
-pthread_t philosopher[NUM_OF_PHILOSOPHERS];
-pthread_mutex_t chopstick[NUM_OF_PHILOSOPHERS];
+#define LEFT (n + NUM_OF_PHILOSOPHERS - 1) % NUM_OF_PHILOSOPHERS // Calcule the left position in a circular array
+#define RIGHT (n + 1) % NUM_OF_PHILOSOPHERS // Calcule the right position in a circular array
+
+typedef enum {
+    statusThinking,
+    statusWaiting,
+    statusEating
+} status;
+
+status states[NUM_OF_PHILOSOPHERS];
+pthread_t philosophers[NUM_OF_PHILOSOPHERS];
+pthread_mutex_t chopsticks[NUM_OF_PHILOSOPHERS];
+pthread_mutex_t mutex;
+
+void printStatus() {
+    fprintf(stdout, "\nPrinting the status of all 5 philosophers:\n");
+    fflush(stdout);
+    int i;
+    for (i = 0; i < NUM_OF_PHILOSOPHERS; ++i) {
+        switch (states[i]) {
+            case statusThinking:
+                fprintf(stdout, "\nPhilosopher %d is thinking.", i);
+                break;
+            case statusWaiting:
+                fprintf(stdout, "\nPhilosopher %d is waiting to eat.", i);
+                break;
+            case statusEating:
+                fprintf(stdout, "\nPhilosopher %d is eating.", i);
+                break;
+        }
+        fflush(stdout);
+    }
+}
 
 void think(int n) {
-    //fprintf(stdout, "Philosopher %d is thinking\n", n);
-    //fflush(stdout);
+    states[n] = statusThinking;
     usleep(25);
 }
 
-bool canEat(int n) {
-    return true;
+void eatIfPossible(int n) {
+    printStatus();
+    if (states[n] == statusWaiting && states[LEFT] != statusEating && states[RIGHT] != statusEating) {
+        states[n] = statusEating;
+        fprintf(stdout, "\nPhilosopher %d has started eating now.\n", n);
+        fflush(stdout);
+        pthread_mutex_unlock(&chopsticks[n]);
+    }
 }
 
-void printStatus() {
-    fprintf(stdout, "Printing the status of all 5 philosophers");
-    fflush(stdout);
+void takeChopsticks(int n) {
+    pthread_mutex_lock(&mutex);
+    states[n] = statusWaiting;
+    eatIfPossible(n);
+    pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&chopsticks[n]);
 }
 
-void *func(int n) {
+void putChopsticks(int n) {
+    pthread_mutex_lock(&mutex);
+    states[n] = statusThinking; // We shouldn't call think(); here
+    eatIfPossible(LEFT);
+    eatIfPossible(RIGHT);
+    pthread_mutex_unlock(&mutex);
+}
+
+void *thinkAndEat(int n) {
     int i;
     for (i = 0; i < NUM_OF_ITERATIONS; ++i) {
         think(n);
-        // When philosopher 0 is eating, he takes forks 0 and 1
-        // When philosopher 1 is eating, he takes forks 1 and 2
-        // When philosopher 2 is eating, he takes forks 2 and 3
-        // When philosopher 3 is eating, he takes forks 3 and 4
-        // When philosopher 4 is eating, he takes forks 4 and 0
-        pthread_mutex_lock(&chopstick[n]);
-        pthread_mutex_lock(&chopstick[(n+1)%NUM_OF_PHILOSOPHERS]);
-        fprintf(stdout, "Philosopher %d is eating\n",n);
-        fflush(stdout);
-        sleep(3);
-        pthread_mutex_unlock(&chopstick[n]);
-        pthread_mutex_unlock(&chopstick[(n+1)%5]);
+        takeChopsticks(n);
+        putChopsticks(n);
     }
-    
     fprintf(stdout, "Philosopher %d finished eating %d times\n", n, NUM_OF_ITERATIONS);
-    fflush(stdout);
-        
+    fflush(stdout);   
     return(NULL);
 }
 
-int main() {
+void cleanEatingStates() {
+    int i;
+    for (i = 0; i < NUM_OF_PHILOSOPHERS; ++i) {
+        states[i] = statusThinking;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    cleanEatingStates();
+    
     int i;
     // Initialize 5 mutex
     for (i = 0; i < NUM_OF_PHILOSOPHERS; i++) {
-        pthread_mutex_init(&chopstick[i], NULL);
+        pthread_mutex_init(&chopsticks[i], NULL);
     }
+    pthread_mutex_init(&mutex, NULL);
     
     // Create 5 threads and execute the function on each of them
     for (i = 0; i < NUM_OF_PHILOSOPHERS; ++i) {
-        pthread_create(&philosopher[i], NULL, (void *)func, (void *)i);
+        pthread_create(&philosophers[i], NULL, (void *)thinkAndEat, (void *)i);
     }
     
     // Waits for the termination of all 5 threads
     for (i = 0; i < NUM_OF_PHILOSOPHERS; ++i) {
-        pthread_join(philosopher[i],NULL);
+        pthread_join(philosophers[i],NULL);
     }
     
     // Destroy all 5 mutex
     for (i = 0; i < NUM_OF_PHILOSOPHERS; ++i) {
-        pthread_mutex_destroy(&chopstick[i]);
+        pthread_mutex_destroy(&chopsticks[i]);
     }
+    pthread_mutex_destroy(&mutex);
     
     return 0;
 }
