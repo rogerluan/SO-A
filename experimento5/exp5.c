6dc26392f334c24531011b2b8a151a47890de3a3
Roger Oba <rogerluan.oba@gmail.com>
2017-04-19 22:37:05 -0300

    Created exp5 folder and first file

---
+++ experimento5/exp5.c
@@ -0,0 +1,19 @@
+//
+//  exp5
+//
+//  Created on Apr 19th 2017
+//
+//  ALEX VENTURINI  15294739
+//  BRUNO PEDROSO   12662136
+//  LUAN BONOMI     15108780
+//  PEDRO CATALINI  15248354
+//  ROGER OBA       12048534
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
