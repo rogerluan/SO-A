b082a95bc9ff5d61d438d240a00453e72dd80a32
Roger Oba <rogerluan.oba@gmail.com>
2017-05-29 17:46:42 -0300

    Created testing script and

--- experimento7/exp7/exp7.c
+++ experimento7/exp7/exp7.c
@@ -38,23 +38,22 @@
  Feche o descritor
  */
 
+#include <stdio.h>      // printf
+//#include <sys/stat.h>
+#include <fcntl.h>      // open options
+#include <stdbool.h>    // boolean type
+#include <errno.h>      // errno constant
 
-
-#include <stdio.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdbool.h>
-#include <errno.h>
-
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <stdlib.h>     // malloc
+#include <string.h>     // string helper functions (strcmp, strcpy)
+//#include <sys/types.h>
+#include <unistd.h>     // lseek, open, etc
 
 #define FILE_NAME_MAX_SIZE 30
 
 typedef enum {
-    FunctionOpen = 1,
+    FunctionNull,
+    FunctionOpen,
     FunctionWrite,
     FunctionRead,
     FunctionSeek,
@@ -70,7 +69,7 @@ int main(int argc, char *argv[]) {
     int argumentsRead = 0;
     
     if (strcmp(argv[1], "open") != 0) {
-        printf("O primeiro argumento deve abrir um arquivo. Abortando.\n");
+        printf("O primeiro argumento deve abrir um arquivo. Abortando.\n\n\n");
         return -1;
     }
     
@@ -84,6 +83,7 @@ int main(int argc, char *argv[]) {
         Function function = getFunction(argv[argumentPosition]);
         
         switch (function) {
+            case FunctionNull: break;
             case FunctionOpen: {
                 printf("Executing open function.\n");
                 if ((strcmp(argv[argumentPosition + 1], "criar") == 0) || (strcmp(argv[argumentPosition + 1], "new") == 0)) {
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
                 size_t bytesToWrite;
                 char *buffer;
 
-                if (argumentCount == 1 || (argumentCount > 1 && (getFunction(argv[argumentPosition + 1]) == 0))) { // There are no more parameters after write function
+                if (argumentCount == 1 || (argumentCount > 1 && (getFunction(argv[argumentPosition + 1]) == FunctionNull))) { // There are no more parameters after write function
                     printf("Print last buffer.");
                     bytesToWrite = lastSizeRead;
                     buffer = lastDataRead;
@@ -167,13 +167,16 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
-//        printf("argCount: %d, arg pos: %d\n", argumentCount, argumentPosition);
-        argumentCount -= argumentsRead; // argumentCount = argumentCount - number of arguments read + 1(inc)
+        argumentCount -= argumentsRead;
     }
+    printf("\n\n");
     return 1;
 }
 
-/// Open Helper Functions
+/**
+ * Helper Functions
+ *
+ */
 int openFile(char *fileName, bool shouldCreate) {
     int descriptor = -1;
     if (shouldCreate) {
@@ -188,25 +191,13 @@ int openFile(char *fileName, bool shouldCreate) {
     return descriptor;
 }
 
-//if (strcmp(argv[argumentPosition], "open") == 0) { function = FunctionOpen; }
-//else if (strcmp(argv[argumentPosition], "write") == 0) { function = FunctionWrite; }
-//else if (strcmp(argv[argumentPosition], "read") == 0) { function = FunctionRead; }
-//else if (strcmp(argv[argumentPosition], "seek") == 0) { function = FunctionSeek; }
-//else if (strcmp(argv[argumentPosition], "close") == 0) { function = FunctionClose; }
-//else {
-//    printf("Lendo argumento que nao eh funcao!");
-//}
-
 Function getFunction(char *arg) {
-    Function function = 0;
+    Function function = -1;
     if (strcmp(arg, "open") == 0) { function = FunctionOpen; }
     else if (strcmp(arg, "write") == 0) { function = FunctionWrite; }
     else if (strcmp(arg, "read") == 0) { function = FunctionRead; }
     else if (strcmp(arg, "seek") == 0) { function = FunctionSeek; }
     else if (strcmp(arg, "close") == 0) { function = FunctionClose; }
-    else {
-        printf("Lendo argumento que nao eh funcao!");
-    }
     return function;
 }
 
@@ -216,181 +207,3 @@ void print(const char * __restrict fmt, ...) {
 #endif
 }
 
-//#ifdef DEBUG
-//        printf("filePath: %s", fileName);
-//        strcat(fileName, "/Users/rogerluan/Documents/Projects/SO-A/experimento7/exp7/");
-//        printf("filePath: %s", fileName);
-//#endif
-
-// Pegar o proximo argumento
-// Checar se o argumento é uma funcao
-// se for uma funcao
-//      executar a função anterior
-//      entrar no switch case daquela funcao
-//      ler o proximo argumento
-// Se não for uma funcão
-//      atribuir este valor lido na variável correta
-// Ler o proximo argumento
-
-
-//  ALEX VENTURINI  15294739
-//  BRUNO PEDROSO   12662136
-//  LUAN BONOMI     15108780
-//  PEDRO CATALINI  15248354
-//  ROGER OBA       12048534
-
-// open novo_ou_velho nome_do_arquivo
-// write o_que_for_digitado quantidade_de_caracteres_em_int
-// seek quantidade_de_posicao_que_deve_dar_seek
-// read quantidade_de_caracteres_que_vai_ler
-// close (soh close)
-
-// O_WRONLY SOMENTE PARA ESCRITA
-// O_RDONLY SOMENTE PARA LEITURA
-// O_RDWR ESCRITA E LEITURA
-
-// argv[1] tem q ser open para abrir o arquivo!
-
-/*
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int open_func(char *velho_novo, char *nome_arq, int qual) {
-    int i = 0;
-    char compara[1][10];
-    int d = 0;
-    
-    strcpy(compara[0], velho_novo);
-    
-    if (strcmp(compara[0], "novo") == 0) {
-        printf("novo\n");
-        printf("qual = %d\n", qual);
-        if (qual == 1) {
-            printf("write\n");
-            // ativa a funcao write
-            d = open (nome_arq, O_WRONLY | O_CREAT);
-            return d;
-        } else {
-            if (qual == 2) {
-                printf("read\n");
-                // ativa a funcao read
-                d = open (nome_arq, O_RDONLY | O_CREAT);
-                return d;
-            } else {
-                if(qual == 3){
-                    printf("write e read\n");
-                    d = open (nome_arq, O_RDWR | O_CREAT);
-                    return d;
-                }else{
-                    // ativa nada e da erro
-                    return -1;
-                }
-            }
-        }
-    } else {
-        if (strcmp(compara[0], "velho") == 0) {
-            printf("velho\n");
-            if (qual == 1) {
-                printf("write\n");
-                // ativa a funcao write
-                d = open(nome_arq, O_WRONLY);
-                return d;
-            } else {
-                if (qual == 2) {
-                    printf("read\n");
-                    // ativa a funcao read
-                    d = open(nome_arq, O_RDONLY);
-                    return d;
-                } else {
-                    if (qual == 3) {
-                        printf("write e read\n");
-                        d = open(nome_arq, O_RDWR);
-                        return d;
-                    } else {
-                        // ativa nada e da erro
-                        return -1;
-                    }
-                }
-            }
-        }
-    }
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    int i = 1, j = 0, k = 0;
-    int d = 0;
-    bool writeFlag = false;
-    bool readFlag = false;
-    char leu[100];
-    if (strcmp(argv[i], "open") == 0) {
-        i++;
-        
-        // TODO: rever logica de identificacao da funcao open se é read ou write
-        while (i < argc) {
-            if (strcmp(argv[i], "write") == 0) {
-                writeFlag = true;
-            } else if (strcmp(argv[i], "read") == 0) {
-                readFlag = true;
-            }
-            i++;
-        }
-        
-        if (writeFlag && readFlag) {
-            d = open_func(argv[2], argv[3], 3);
-        } else {
-            if (writeFlag && !readFlag) {
-                d = open_func(argv[2], argv[3], 1);
-            } else if (!writeFlag && readFlag) {
-                d = open_func(argv[2], argv[3], 2);
-            }
-        }
-        i = 3;
-        while(i < argc) {
-            //write
-            if (strcmp(argv[i], "write") == 0) {
-                write(d, argv[i + 2], atoi(argv[i + 1]));
-                i++;
-            } else {
-                //read
-                if(strcmp(argv[i], "read") == 0) {
-                    
-                    read(d, leu, atoi(argv[i + 1]));
-                    k = atoi(argv[i + 1]);
-                    for(j = 0; j < k; j++){
-                        printf("%c", leu[j]);
-                    }
-                    printf("\n");
-                    i++;
-                }
-                else {
-                    // Seek
-                    if (strcmp(argv[i], "seek") == 0) {
-                        // Falta a funcao seek
-                        i++;
-                    } else {
-                        // Close
-                        if (strcmp(argv[i], "close") == 0) {
-                            i++;
-                        } else {
-                            i++;
-                        }
-                    }
-                }
-            }
-        }
-    } else {
-        if (d == 0) {
-            printf("Erro ao abrir o arquivo\n");
-        }
-        printf("Impossivel de abrir o arquivo!\n");
-    }
-    close(d);
-}
-*/
---
+++ experimento7/exp7/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Executing the tests..."
+
+echo "Compiling the program..."
+clang exp7.c -o exp7
+
+echo "Cleaning the directory from previous files..."
+rm novo
+rm novo1
+rm novo2
+
+
+echo "Running the 1st test:"
+./exp7 write "aaabcdefghij" 12 seek 0 write "##" 2 >> test_results.txt
+
+echo "Running the 2nd test:"
+./exp7 open new "novo" write "aaabcdefghij" 12 seek 0 write "##" 2 close >> test_results.txt
+
+echo "Running the 3rd test:"
+./exp7 open new "novo1" write "aaabcdefghij" 12 seek 100 write "##" 2 close >> test_results.txt
+
+echo "Running the 4th test:"
+./exp7 open new "novo2" write "aaabcdefghij" 12 seek 100000 write "##" 2 seek 100001 write "@@@@@" 5 >> test_results.txt
+
+echo "Running the 5th test:"
+./exp7 open old "novo2" seek 50000 read 2 seek 100003 write >> test_results.txt
+
+echo "Finished testing everything!"
